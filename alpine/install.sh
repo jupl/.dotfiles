cb4e3dc2fcd218fd1fac3372ae5079f5f8d3d17b
jupl <juan.placencia.512@gmail.com>
2018-05-17 17:54:16 -0500

    [alpine] Preliminary support for alpine

---
+++ alpine/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+source ../helpers
+
+if [[ "$OS" == alpine ]]; then
+  echo '!!! This script is only available for Alpine'
+  exit 1
+fi
+
+echo '--- Setting up apk'
+# TODO add community package by modifying /etc/apk/repositories
+apk update
+apk upgrade
+
+install_deps
