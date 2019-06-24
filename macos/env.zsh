393748850ef3b1d8a7c6fb0585ded60fcdb69a61
jupl <juan.placencia.512@gmail.com>
2019-06-24 16:00:00 -0500

    [macos] Add android sdk env support

--- macos/env.zsh
+++ macos/env.zsh
@@ -1,3 +1,7 @@
 if [[ "$OS" == macos ]]; then
   export CLICOLOR=1
+  export ANDROID_SDK_ROOT='/usr/local/share/android-sdk'
+  if [[ ! -d "$ANDROID_SDK_ROOT" ]]; then
+    unset ANDROID_SDK_ROOT
+  fi
 fi
